ec7598f1d70bcd4c3ac2e7fe6a52bc04dd97dec3
Roman Kruglov <kruglov.pechatnik@yandex.ru>
2023-06-28 00:05:22 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-import { useDispatch, useSelector } from 'react-redux'
-
-import { plateScaleSelector } from './store/slices/plateScaleSlice'
-import { resetPlateScale } from './store/slices/plateScaleSlice'
-
-import { formsRawDataSelector } from './store/slices/printFormsDataSlice'
-
 import './App.css'
 import Header from './components/Header'
 import Plate from './components/Plate'
@@ -14,8 +7,6 @@ import UnusedPrintFormsPreview from './components/UnusedPrintFormsPreview'
 import { nanoid } from 'nanoid'
 
 function App() {
-  const rawData = useSelector((state) => state.printFormsData.formsRawData)
-
   return (
     <div className="App">
       <Header />
