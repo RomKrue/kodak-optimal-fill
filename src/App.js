5c3cefcd46ff21ba6868d6b9db083938ec559b7d
Roman <romolusis@yandex.ru>
2023-07-07 01:55:18 +0400

    upd

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
   return (
     <div className="App" ref={appRef}>
       <Header />
+      <section className="main-content"></section>
       <UnusedPrintFormsPreview />
       {/*
       
